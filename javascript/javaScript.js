d88493cf297bc928152137fc6624d8313dc8bb22
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-29 20:16:11 -0300

    M

--- javascript/javaScript.js
+++ javascript/javaScript.js
@@ -1,38 +1,30 @@
 console.log('Hola Mundo')
 
-// lo primero que hacemos es traernos todas las participantes
+//Selecciones
 
-function fetchParticipantes (url) {
-    document.getElementById('loader').classList.add('show');
-    
-    fetch(`https://www.nokeynoshade.party/api/queens/${url}`)
-      .then(res => res.json()) // interprete los datos como json
-      .then(data => {
-        const ul = document.querySelector('ul#listado-participantes');
-        let lis = '';
-  
-        for (let i = 0; i < data.length; i++) {
-          lis += `
-            <li>
-              <img src="${data[i].image_url}" />
-              <p>${data[i].name}</p>
-              <p>${data[i].winner ? 'Si' : 'No'}</p>
-            </li>
-          `;
-        }
-  
-        document.getElementById('loader').classList.remove('show');
-        ul.innerHTML = lis;
-      });
-  }
-  
-  
-  document.getElementById('all').onclick = function () {
-    fetchParticipantes('all');
-  }
-  // http://www.nokeynoshade.party/api/queens/winners
-  document.getElementById('winners').onclick = function () {
-    fetchParticipantes('winners')
-  }
-  
-  fetchParticipantes('all');
+const participantes = document.querySelector('.participantes');
+console.log(participantes)
+
+
+const all = `https://www.nokeynoshade.party/api/queens`
+const winners = `https://www.nokeynoshade.party/api/queens/winners`
+
+fetch(all)
+  .then(res => res.json())
+  .then(data => {
+
+    let lis = '';
+
+    for (let i = 0; i < data.length; i++) {
+      lis += `
+        <li class"cadaParticipante">
+        <div class="img">
+          <img src="${data[i].image_url}" />
+          </div>
+          <p class="nombre">${data[i].name}</p>
+        </li>
+      `;
+    }
+    participantes.innerHTML = lis;
+            
+        })
